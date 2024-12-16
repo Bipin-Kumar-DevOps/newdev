4674a352223305d5e2295ebb9d6f1376753e6164
bipin <kumarbipin247@gmail.com>
2024-12-16 19:38:19 +0530

    updated code

--- env/dev/provider.tf
+++ env/dev/provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "4.11.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "dev-rg2"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "devbkstg01"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "dev"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "dev.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
 }
 
 provider "azurerm" {
