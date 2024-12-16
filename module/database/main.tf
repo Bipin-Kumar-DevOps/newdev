81061030f29bbfd60e36bc26b360ed67e4fe336b
bipin <kumarbipin247@gmail.com>
2024-12-16 22:02:09 +0530

    new code

--- module/database/main.tf
+++ module/database/main.tf
@@ -47,4 +47,5 @@ data "azurerm_mssql_server" "sqlserver" {
   for_each = var.sqlserver  
   name                = each.value.sqlserver_name
   resource_group_name = each.value.resource_group_name
+  depends_on = [ azurerm_mssql_server.sqlserver ]
 }
