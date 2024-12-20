047c134473209ef67815c8c80ec1ad6db023fb8b
bipin <kumarbipin247@gmail.com>
2024-12-20 14:17:58 +0530

    updated stg acc

--- env/dev/provider.tf
+++ env/dev/provider.tf
@@ -6,7 +6,8 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "dev-rg2"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    resource_group_name  = "MC_dev-rg1_aks-dev-cluster_eastus
+"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
     storage_account_name = "devbkstg01"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "dev"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "dev.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
