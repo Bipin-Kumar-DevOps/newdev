d1f229fe57c2a65801bb944410fd2da189047db0
bipin <kumarbipin247@gmail.com>
2024-12-20 14:27:56 +0530

    updated code

--- module/nic/main.tf
+++ module/nic/main.tf
@@ -19,7 +19,7 @@ data "azurerm_subnet" "data_subnet" {
   virtual_network_name = each.value.virtual_network_name
   resource_group_name  = each.value.resource_group_name
 }
-data "azurerm_public_ip" "data-pip" {
+data "azurerm_public_ip" "pip" {
     for_each = var.nic
   name                = each.value.pip_name
   resource_group_name = each.value.resource_group_name
